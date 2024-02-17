5bba4b1ab9cf6be897b378cf66c3cc09d77f1af0
Epereira2021576 <epereira-2021576@kinal.edu.gt>
2024-02-16 20:46:55 -0600

    Revert "Uploaded project"
    This reverts commit e96790800c8dae02b82a425f8cdc6531bceed49f.

--- app.js
+++
@@ -1,8 +0,0 @@
-const Server = require('./models/server');
-
-require('dotenv').config();
-
-const server = new Server();
-
-// Calling the listen() method from the Server class to start the server
-server.listen();
